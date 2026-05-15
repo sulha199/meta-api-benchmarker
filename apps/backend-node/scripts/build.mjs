ba241962bb4e925d803a26a43f47d88ff231844d
Shulha Yahya <sulha199@gmail.com>
2026-05-15 18:55:22 +0700

    chore: fix vercel error 404

--- apps/backend-node/scripts/build.mjs
+++ apps/backend-node/scripts/build.mjs
@@ -15,7 +15,7 @@ await esbuild.build({
   bundle: true,
   platform: "node",
   format: "esm",
-  outfile: "api/graphql.js",
+  outfile: "../../api/graphql.js",
   external: external.filter((dep) => !dep.startsWith("@repo/")),
   loader: {
     ".graphql": "text",
